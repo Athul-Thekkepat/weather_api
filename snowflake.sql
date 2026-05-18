b5e03c7dc93c4dfe3034be10f5c7cc2c07ea1b34
Athul-Thekkepat <tpnandu8@gmail.com>
2026-05-18 17:40:00 +0530

    S3 to Stage and Credentials

--- snowflake.sql
+++ snowflake.sql
@@ -4,3 +4,10 @@ USE DATABASE WEATHERR_DB;
 CREATE TABLE WEATHER_DATA(
     data variant
 );
+
+Create or replace stage my_s3_stage
+URL = 's3://weather-data-bucket-athul/'
+Credentials = (
+    AWS_KEY_ID = os.getenv('AWS_KEY_ID')
+    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+);
