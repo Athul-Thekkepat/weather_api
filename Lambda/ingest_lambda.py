859880d257af5abd31aeabca06f753e06b15c09c
Athul-Thekkepat <tpnandu8@gmail.com>
2026-05-18 16:24:42 +0530

    Add AWS Lambda weather fetch script

---
+++ Lambda/ingest_lambda.py
@@ -0,0 +1,16 @@
+import json
+import urllib.request
+import boto3
+from datetime import datetime
+
+def lambda_handler(event, context):
+    
+    api_key = "04c20c5de85f796888612a01e8d749d9"
+    city = "Kozhikode"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+
+    print(data) 
