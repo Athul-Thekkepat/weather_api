257ae8a10a40365a252b5de7ccab02c2166a67f6
Athul-Thekkepat <tpnandu8@gmail.com>
2026-05-18 17:11:46 +0530

    Export DynamoDB stream records to S3

--- Lambda/process_lambda.py
+++ Lambda/process_lambda.py
@@ -3,3 +3,22 @@ import boto3
 
 s3 = boto3.client('s3')
 BUCKET_NAME = "weather-data-bucket-athul"
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        new_image = record['dynamodb']['NewImage']
+
+        item = {
+            "city": new_image['city']['S'],
+            "time": new_image['time']['S'],
+            "temperature": new_image['temperature']['S'],
+            "description": new_image['description']['S']
+        }
+
+        file_name = f"{item['city']}_{item['time']}.json"
+
+        s3.put_object(
+            Bucket=BUCKET_NAME,
+            Key=file_name,
+            Body=json.dumps(item)
+        )
